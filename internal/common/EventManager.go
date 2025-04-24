4065ccadcfc68a83c471f0d2adecff91f5c7baf9
AYn0nyme <aynoblox@gmail.com>
2025-04-23 23:02:42 -0400

    Replaced ...interface{} with ...any in EventManager.go

--- internal/common/EventManager.go
+++ internal/common/EventManager.go
@@ -3,23 +3,23 @@ package common
 import "sync"
 
 type EventManager struct {
-	Handlers map[string][]func(args ...interface{})
+	Handlers map[string][]func(args ...any)
 	Mutex    sync.RWMutex
 }
 
 func NewEventManager() *EventManager {
 	return &EventManager{
-		Handlers: make(map[string][]func(args ...interface{})),
+		Handlers: make(map[string][]func(args ...any)),
 	}
 }
 
-func (e *EventManager) On(Event string, Handler func(args ...interface{})) {
+func (e *EventManager) On(Event string, Handler func(args ...any)) {
 	e.Mutex.Lock()
 	defer e.Mutex.Unlock()
 	e.Handlers[Event] = append(e.Handlers[Event], Handler)
 }
 
-func (e *EventManager) Emit(Event string, Args ...interface{}) {
+func (e *EventManager) Emit(Event string, Args ...any) {
 	e.Mutex.Lock()
 	defer e.Mutex.Unlock()
 
