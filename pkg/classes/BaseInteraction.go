bedcb98cbf6e2e982f728ba121250a588d1b9b3d
AYn0nyme <aynonyme@proton.me>
2025-05-28 16:58:37 -0400

    added Data,Guild to BaseInteraction

--- pkg/classes/BaseInteraction.go
+++ pkg/classes/BaseInteraction.go
@@ -7,6 +7,13 @@ type BaseInteraction struct {
 	Token  string                `json:"token"`
 	Member GuildMember           `json:"member"`
 	ID     string                `json:"id"`
+	Guild  Guild                 `json:"guild"`
+	Data   baseInteractionData   `json:"data"`
+}
+type baseInteractionData struct {
+	Type types.InteractionType `json:"type"`
+	Name string                `json:"name"`
+	ID   string                `json:"id"`
 }
 
 type BaseComponent interface {
