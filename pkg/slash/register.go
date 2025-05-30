af2d3ce9af49626abc92d96c68c0e0062955bc74
AYn0nyme <aynonyme@proton.me>
2025-05-30 09:56:34 -0400

    added RegisterGlobalCommands

--- pkg/slash/register.go
+++ pkg/slash/register.go
@@ -44,3 +44,35 @@ func RegisterGuildCommands(ApplicationID string, Commands []classes.SlashCommand
 
 	return nil
 }
+
+func RegisterGlobalCommands(ApplicationID string, Commands []classes.SlashCommandData) error {
+	for _, cmd := range Commands {
+		if cmd.Name == "" || cmd.Description == "" || cmd.Type == 0 {
+			return errors.New("missing required fields in SlashCommandData")
+		}
+		reqBody, err := json.Marshal(cmd)
+		if err != nil {
+			return err
+		}
+
+		req, err := http.NewRequest("POST", fmt.Sprintf("%s/applications/%s/commands", classes.API_URL, ApplicationID), bytes.NewReader(reqBody))
+		if err != nil {
+			return err
+		}
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bot "+os.Getenv("GODISCORD_TOKEN"))
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		if res.StatusCode != 200 {
+			return errors.New(string(body))
+		}
+	}
+
+	return nil
+}
