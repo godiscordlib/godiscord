87b6765b43ff3f2c763383ebfc53121c5dc08cb4
AYn0nyme <aynonyme@proton.me>
2025-12-07 12:28:17 -0400

    fix: added type to useroption

--- pkg/classes/SlashCommandUserOption.go
+++ pkg/classes/SlashCommandUserOption.go
@@ -1,5 +1,14 @@
 package classes
 
-type SlashCommandUserOption struct  {
+import (
+	"github.com/godiscordlib/godiscord/pkg/enums"
+	"github.com/godiscordlib/godiscord/pkg/types"
+)
+
+type SlashCommandUserOption struct {
 	SlashCommandOptionInt
+}
+
+func (scso SlashCommandUserOption) GetSCOType() types.SlashCommandOptionType {
+	return enums.SlashCommandOptionType.String
 }
