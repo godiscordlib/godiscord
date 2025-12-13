3c856a6a452ebe65ff087bfaeedaa1ff2617c14e
AYn0nyme <aynonyme@proton.me>
2025-12-13 10:29:08 -0400

    fix: all json of User got removed for some reason

--- pkg/classes/User.go
+++ pkg/classes/User.go
@@ -2,12 +2,12 @@ package classes
 
 type User struct {
 	Base
-	Username      string
-	Discriminator string
-	Bot           bool
-	Global_Name   string
-	AvatarHash    string
-	BannerHash    string
+	Username      string `json:"username"`
+	Discriminator string `json:"discriminator"`
+	Bot           bool   `json:"bot"`
+	Global_Name   string `json:"display_username"`
+	AvatarHash    string `json:"avatar"`
+	BannerHash    string `json:"banner"`
 	AccentColor   int
 	Locale        string
 	Flags         int
